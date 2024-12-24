2342e855390e8012a2aa9bd2097e7f0c3000b722
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-12-24 13:18:39 -0500

    Privacidad de datos

--- database/seeders/UsuariosSeeder.php
+++ database/seeders/UsuariosSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,19 +14,19 @@ class UsuariosSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Marvin Campos',
-            'odoo_uid' => 2, // CADA USUARIO TIENE UN UID ÚNICO DEBES IDENTIFICARLO EN EL SISTEMA DE ODOO
-            'email' => 'marvinhectorcamposdeza@gmail.com',
-            'token' => '7942d56bb14e4407fbad55e3f30c0354bcd99a51',
-            'password' => Hash::make('987654321'),
+            'name' => env('USER1_NAME'),
+            'odoo_uid' => env('USER1_ODOO_UID'),
+            'email' => env('USER1_EMAIL'),
+            'token' => env('USER1_TOKEN'),
+            'password' => Hash::make(env('USER1_PASSWORD')),
         ]);
 
         User::create([
-            'name' => 'Jhamil Crispin',
-            'odoo_uid' => 6, // CADA USUARIO TIENE UN UID ÚNICO DEBES IDENTIFICARLO EN EL SISTEMA DE ODOO
-            'email' => 'j99crispin@gmail.com',
-            'token' => 'b7946a934c26a7c939127b526096864e29b1dafa',
-            'password' => Hash::make('987654321'),
+            'name' => env('USER2_NAME'),
+            'odoo_uid' => env('USER2_ODOO_UID'),
+            'email' => env('USER2_EMAIL'),
+            'token' => env('USER2_TOKEN'),
+            'password' => Hash::make(env('USER2_PASSWORD')),
         ]);
     }
 }
